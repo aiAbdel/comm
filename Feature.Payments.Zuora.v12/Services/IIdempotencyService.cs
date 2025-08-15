b84e018d6d7732011973377835226c99ab8a8c5f
aiAbdel <ai.abdel.life@gmail.com>
2025-08-15 15:57:01 -0400

    Delete Feature.Payments.Zuora.v12 directory

--- Feature.Payments.Zuora.v12/Services/IIdempotencyService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-namespace Your.Feature.Payments.Services
-{
-  public interface IIdempotencyService
-  {
-    Task<string> GetOrCreateKeyAsync(string accountNumber, string ratePlanId, string chargeId, int quantity);
-    Task<bool> ClearKeyAsync(string accountNumber, string ratePlanId, string chargeId, int quantity);
-  }
-}
